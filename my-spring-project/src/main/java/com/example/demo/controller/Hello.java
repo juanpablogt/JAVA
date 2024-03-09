bb96fef22fe8e10ccf3a4f0ac5d4559d6882420f
JPabloGozalezT <54081160+juanpablogt@users.noreply.github.com>
2024-03-09 18:25:57 -0500

    Add files via upload

---
+++ my-spring-project/src/main/java/com/example/demo/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hallo;Ich bin Juan Pablo Gonzalez Trejos, Ich komme aus kolumbien,ich bin 24 Jahre alt, ich bin student an der Hochschule Bonn-Rhein-Sieg, ich studiere Informatik und ich bin im 5. Semester. Ich spreche Spanisch, Englisch und Deutsch. Ich bin ein sehr freundlicher und fleißiger Mensch. Ich bin sehr interessiert an der Technologie und ich bin sehr motiviert, um in der Zukunft ein guter Informatiker zu werden. Ich bin sehr glücklich, dass ich in Deutschland studieren kann. Ich bin sehr dankbar für diese Gelegenheit. Vielen Dank.";
+    }
+}
