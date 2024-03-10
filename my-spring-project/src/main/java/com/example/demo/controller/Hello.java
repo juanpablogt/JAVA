5a62a12f40d36d56eca0c8ba606bdf174875f6bb
JPGonzalezT <juapabgonzalez@utp.edu.co>
2024-03-09 19:21:30 -0500

    Inicio

--- my-spring-project/src/main/java/com/example/demo/controller/Hello.java
+++ my-spring-project/src/main/java/com/example/demo/controller/Hello.java
@@ -9,6 +9,6 @@ public class Hello {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hallo,Ich bin Juan Pablo Gonzalez Trejos, Ich komme aus kolumbien,ich bin 24 Jahre alt, ich bin student an der Hochschule Bonn-Rhein-Sieg, ich studiere Informatik und ich bin im 5. Semester. Ich spreche Spanisch, Englisch und Deutsch. Ich bin ein sehr freundlicher und fleißiger Mensch. Ich bin sehr interessiert an der Technologie und ich bin sehr motiviert, um in der Zukunft ein guter Informatiker zu werden. Ich bin sehr glücklich, dass ich in Deutschland studieren kann. Ich bin sehr dankbar für diese Gelegenheit. Vielen Dank.";
+        return "Ich bin Juan Pablo Gonzalez Trejos, Ich komme aus kolumbien,ich bin 24 Jahre alt, ich bin student an der Hochschule Bonn-Rhein-Sieg, ich studiere Informatik und ich bin im 5. Semester. Ich spreche Spanisch, Englisch und Deutsch. Ich bin ein sehr freundlicher und fleißiger Mensch. Ich bin sehr interessiert an der Technologie und ich bin sehr motiviert, um in der Zukunft ein guter Informatiker zu werden. Ich bin sehr glücklich, dass ich in Deutschland studieren kann. Ich bin sehr dankbar für diese Gelegenheit. Vielen Dank.";
     }
 }
